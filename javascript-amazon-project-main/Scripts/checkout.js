3572667b2355a114b65e183a88da1ca6e967309f
Nadil Dulnidu <nzinlud963@gmail.com>
2025-01-07 06:38:41 +0530

    Delete products from HTML

--- javascript-amazon-project-main/Scripts/checkout.js
+++ javascript-amazon-project-main/Scripts/checkout.js
@@ -22,7 +22,7 @@ cart.forEach(item =>{
 
   const cartSummery = `
     
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -110,8 +110,11 @@ document.querySelectorAll('.js-delete-link')
   link.addEventListener('click', () => {
 
     const productId = link.dataset.productId;
-
     removeFromCart(productId);
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+    container.remove();
   
   });
 });
