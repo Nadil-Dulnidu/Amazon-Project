0e8977bc245e97729b967714fbe03b961f63f2aa
Nadil Dulnidu <nzinlud963@gmail.com>
2025-01-10 01:01:01 +0530

    create automated test for a addToCart

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,12 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [];
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage(){
+
+  cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+};
 
 function saveToStorage(){
 
@@ -19,7 +27,7 @@ export function addToCart(productId){
 
   const selectElement = document.querySelector(`.js-quantity-selector-${productId}`);
 
-  const quantity = Number(selectElement.value);
+  let quantity = 1 || Number(selectElement.value);
 
   if (matchingItem) {
     matchingItem.quantity += quantity;
