3059c49e109c21dec9bc252aa6a35e65f4f74415
Nadil Dulnidu <nzinlud963@gmail.com>
2025-01-11 22:48:48 +0530

    Add backend practice file

--- javascript-amazon-project-main/Scripts/checkout.js
+++ javascript-amazon-project-main/Scripts/checkout.js
@@ -1,7 +1,8 @@
 import { renderOrderSummery } from "./checkout/OrderSummery.js";
 import { renderPaymentSummery } from "./checkout/PaymentSummery.js";
 /* import '../data/cart-class.js'; */
-import '../data/car.js';
+/* import '../data/car.js'; */
+import '../data/backend-practice.js';
 
 renderOrderSummery();
 renderPaymentSummery();
